02977c4dd988dde0d6e96e5529492b631e046056
Jumpei-Tanikawa <tjinppsi0815@gmail.com>
2020-07-28 00:04:34 +0900

    deploy

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   process resize_to_fit: [800, 800]
   # end
-
+ 
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
